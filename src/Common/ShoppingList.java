3afb4e1db9b4d1fea812734bd97d60ffe7221ed3
up202004187 <up202004187@up.pt>
2023-11-10 17:13:50 +0000

    don't mind me, just being a cheeky bastard by starting the project before everyone else so that all design decisions effectively pass through me :)

---
+++ src/Common/ShoppingList.java
@@ -0,0 +1,9 @@
+package Common;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class ShoppingList {
+
+    UUID uuid = UUID.randomUUID();
+    HashMap<String, String> items = new HashMap<String, String>();
+}
