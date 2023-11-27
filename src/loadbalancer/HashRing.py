1ce7fde9a0e1f6414914d130b3a57f4f2e440b3f
up202004187 <up202004187@up.pt>
2023-11-27 17:19:42 +0000

    shopping item crdt

--- src/loadbalancer/HashRing.py
+++ src/loadbalancer/HashRing.py
@@ -10,8 +10,9 @@ NUM_REPLICAS = 3
 def hash_function(key):
     return sha256(key.encode('utf-8')).hexdigest()
 
+
 class HashRing:
-    def __int__(self, nodes=None, v_nodes = NUM_VNODES, hash_fn = hash_function, num_replicas = NUM_REPLICAS):
+    def __int__(self, nodes=None, v_nodes=NUM_VNODES, hash_fn=hash_function, num_replicas=NUM_REPLICAS):
         if nodes is None:
             self.nodes = []
 
@@ -19,6 +20,7 @@ class HashRing:
         self.v_nodes = v_nodes
         self.num_replicas = num_replicas
         self.ring = SortedDict()
+
     def add_node(self, key):
         for i in range(self.v_nodes):
             hashed = hash_function(key + '-' + str(i))
@@ -31,9 +33,8 @@ class HashRing:
             del self.ring[hashed]
         return
 
-    def node_range(self,hashed_key,n):
+    def node_range(self, hashed_key, n):
         keys_before = list(islice(self.ring.irange(maximum=n, reverse=True), n))
         keys_after = list(islice(self.ring.irange(minimum=n), n))
 
-        return keys_before,keys_after
-
+        return keys_before, keys_after
