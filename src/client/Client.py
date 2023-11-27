8f557b3524e75f00dc5cbe55b20815b3cb2723ed
up202004187 <up202004187@up.pt>
2023-11-27 20:07:10 +0000

    server and client

--- src/client/Client.py
+++ src/client/Client.py
@@ -1,36 +1,54 @@
+from uuid import *
 import zmq
 import sys
+
 HOST = '127.0.0.1'
+PORT = 5555
+
 
 class Client:
+
     host: str = None
     port: int = None
+    uuid: UUID = None
     context: zmq.Context = None
     socket: zmq.Context.socket = None
-    
-    def __init__(self, host=HOST, port=5555):
+
+    def __init__(self, host=HOST, port=PORT):
         self.host = host
         self.port = int(port)
+        self.uuid = uuid4()
         self.context = zmq.Context()
-    
+
     def start(self):
-        self.client_socket = self.context.socket(zmq.REQ)
-        self.client_socket.connect(f'tcp://{self.host}:{self.port}')
+        self.socket = self.context.socket(zmq.REQ)
+        self.socket.setsockopt_string(zmq.IDENTITY, str(self.uuid))
+        self.socket.connect(f'tcp://{self.host}:{self.port}')
+        print(f'Client connected to {self.host}:{self.port}')
 
     def send_message(self, message):
-        self.client_socket.send_string(message)
+        formatted_message = {
+            "uuid": str(self.uuid),
+            "message": message
+        }
+        self.socket.send_string(str(formatted_message))
+        self.read_message()
 
-        response = self.client_socket.recv_string()
+    def read_message(self):
+        response = self.socket.recv_string()
         print(f'Received response: {response}')
 
-    def close(self):
-        self.client_socket.close()
+    def stop(self):
+        self.socket.close()
         self.context.term()
 
+
 if len(sys.argv) > 1:
-    while(1):
-        zmq_client = Client(HOST,sys.argv[1])
-        zmq_client.start()
-        
+    client = Client(HOST, int(sys.argv[1]))
+    client.start()
+
+    while True:
+        message = input("Message: ")
+        client.send_message(message)
 else:
     print('Usage: python Client.py <port>')
--- src/server/Server.py
+++ src/server/Server.py
@@ -3,37 +3,51 @@ import threading
 import sys
 
 HOST = '127.0.0.1'
+PORT = 5555
+
 
 class Server:
-    def __init__(self, host=HOST, port=5555):
+
+    host: str = None
+    port: int = None
+    context: zmq.Context = None
+    socket: zmq.Context.socket = None
+
+    def __init__(self, host=HOST, port=PORT):
         self.host = host
         self.port = int(port)
         self.context = zmq.Context()
-        self.server_socket = self.context.socket(zmq.REP)
-        self.server_socket.bind(f'tcp://{self.host}:{self.port}')
-        print(f'Server listening on {self.host}:{self.port}')
-
-    def handle_client(self):
-        while True:
-            message = self.server_socket.recv_string()
-            print(f'Received message: {message}')
-
-            self.server_socket.send_string(f'Response to {message}')
 
     def start(self):
+
+        hostname = f"{self.host}:{self.port}"
+
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.setsockopt_string(zmq.IDENTITY, hostname)
+        self.socket.bind(f'tcp://{hostname}')
+        print(f'Server listening on {hostname}')
+
         try:
             client_handler = threading.Thread(target=self.handle_client)
             client_handler.start()
-            
             client_handler.join()
         except KeyboardInterrupt:
-            print('Server shutting down...')
+            print('KeyboardInterrupt: Server shutting down...')
         finally:
-            self.server_socket.close()
+            self.socket.close()
             self.context.term()
 
+    def handle_client(self):
+        while True:
+            message = self.socket.recv_string()
+
+            print(f'Received message: "{message}"')
+
+            self.socket.send_string(f'Response to "{message}"')
+
+
 if len(sys.argv) > 1:
-    zmq_server = Server(HOST,sys.argv[1])
-    zmq_server.start()
+    server = Server(HOST, int(sys.argv[1]))
+    server.start()
 else:
     print('Usage: python Server.py <port>')
