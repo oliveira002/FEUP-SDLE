aef8ef8f526cd6dde3404d454374b8613647edc7
oliveira002 <deadzin112@gmail.com>
2023-11-30 18:08:14 +0000

    app

---
+++ app/backend/server.js
@@ -0,0 +1,80 @@
+const zmq = require('zeromq');
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const cors = require('cors');
+const server = http.createServer(app);
+
+const io = socketIo(server, { cors: { origin: '*' } }); // Attach Socket.IO to the server
+
+app.use(cors()); // Enable CORS for all routes
+
+const listener = server.listen(4444, function () {
+  console.log('Listening on port ' + listener.address().port);
+});
+
+
+class Client {
+  constructor(io) {
+    // Create a REQ socket
+    this.socket = zmq.socket('req');
+
+    this.uuid = uuidv4();
+    this.socket.identity = 'Client-' + this.uuid.toString('ascii');
+
+    // Connect to the ZeroMQ endpoint
+    const endpoint = 'tcp://127.0.0.1:6666';
+    this.socket.connect(endpoint);
+
+    // Handle messages from the ZeroMQ server
+    this.socket.on('message', (msg) => {
+      const receivedMessage = JSON.parse(msg.toString());
+      console.log('Received message:', receivedMessage);
+
+      io.emit('zmqMessage', receivedMessage);
+    });
+
+    // Handle socket errors
+    this.socket.on('error', (err) => {
+      console.error(`Socket error: ${err}`);
+    });
+
+    // Handle process exit
+    process.on('SIGINT', () => {
+      // Close the ZeroMQ socket gracefully on process exit
+      this.socket.close();
+      process.exit();
+    });
+  }
+
+  sendMessage(body, message_type) {
+    const formattedMessage = {
+      identity: this.uuid.toString(),
+      body: body,
+      type: message_type,
+    };
+
+    // Send the formatted message as JSON to the ZeroMQ server
+    this.socket.send(JSON.stringify(formattedMessage));
+  }
+}
+
+// Pass the Socket.IO instance to the Client constructor
+const clientBackend = new Client(io);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  // Handle messages from the frontend
+  socket.on('frontendMessage', (message) => {
+    // Process the message or send it to ZeroMQ as needed
+    clientBackend.sendMessage(message.body, message.type);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
--- src/loadbalancer/LoadBalancer.py
+++ src/loadbalancer/LoadBalancer.py
@@ -51,7 +51,7 @@ class LoadBalancer:
                 if message['type'] == "GET" or message['type'] == "POST":
                     shopping_list = message['body']
                     key, value = self.ring.get_server(shopping_list)
-                    request = [value.encode("utf-8"), b"", identity.encode("utf-8"), b"", json.dumps(message).encode("utf-8")]
+                    request = [self.workers[0].encode("utf-8"), b"", identity.encode("utf-8"), b"", json.dumps(message).encode("utf-8")]
                     self.backend.send_multipart(request)
 
             if self.backend in sockets:
@@ -63,7 +63,7 @@ class LoadBalancer:
 
                 if message['type'] == 'CONNECT':
                     self.ring.add_node(identity)
-                    #self.workers.append(identity)
+                    self.workers.append(identity)
 
                 else:
                     request = [message['identity'].encode("utf-8"), b"", json.dumps(message).encode("utf-8")]
