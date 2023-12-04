6e84e20fdeb14de0fe273425dfddf6a0cd7883a1
oliveira002 <deadzin112@gmail.com>
2023-12-04 21:10:07 +0000

    Replicated

--- src/server/Server.py
+++ src/server/Server.py
@@ -41,6 +41,8 @@ class Server:
     socket: zmq.Context.socket = None
 
     def __init__(self, host=HOST, port=PORT):
+        self.poller = None
+        self.socket_neigh = None
         self.ring = None
         self.hostname = None
         self.host = host
@@ -53,18 +55,30 @@ class Server:
         self.socket = self.context.socket(zmq.DEALER)
         self.socket.identity = u"Server@{}".format(hostname).encode("ascii")
         self.socket.connect(f'tcp://{BROKER}')
+
+        self.socket_neigh = self.context.socket(zmq.DEALER)
+        self.socket_neigh.identity = u"Server2@{}".format(hostname).encode("ascii")
+        self.socket_neigh.bind(f"tcp://127.0.0.1:{self.port}")
+
         logger.info(f'Server connected to Broker at {BROKER}')
         self.poller = zmq.Poller()
         self.poller.register(self.socket, zmq.POLLIN)
+        self.poller.register(self.socket_neigh, zmq.POLLIN)
         self.ring = None
 
-        self.send_message("Initial Setup", "CONNECT")
+        self.send_message(self.socket, "Initial Setup", "CONNECT")
 
         while True:
             socks = dict(self.poller.poll())
+
             if self.socket in socks and socks[self.socket] == zmq.POLLIN:
                 request = self.receive_message()
                 self.handle_request(request)
+
+            if self.socket_neigh in socks and socks[self.socket_neigh] == zmq.POLLIN:
+                request = self.receive_message_neighbour()
+                self.handle_request(request)
+
 
 
 
@@ -79,22 +93,42 @@ class Server:
 
         if req['type'] == 'POST':
             shopping_list = req['body']
-            self.persist_to_json(json.loads(req['body']))
-            self.send_message_response(client_id, "RESOURCE 1")
+            merged = self.persist_to_json(json.loads(req['body']))
+            self.send_message_response(client_id, merged)
 
             _, neighbours = self.ring.get_server(shopping_list)
 
+            self.replicate_data(neighbours, merged)
+
+        if req['type'] == 'REPLICATE':
+            self.persist_to_json(req['body'])
 
 
 
+    def replicate_data(self, neighbours, shopping_list):
+        neighbours = [x.split('@', 1)[-1] for x in neighbours]
+        print(neighbours)
 
-    def send_message(self, body, message_type):
+        for neighbour in neighbours:
+            try:
+                self.socket_neigh.connect(f'tcp://{neighbour}')
+                #print(f"Connected to {neighbour}")
+
+                # Assuming you have a method to send messages
+                self.send_message(self.socket_neigh, shopping_list, "REPLICATE")
+
+                # If needed, wait for a response or handle it asynchronously
+            except Exception as e:
+                print(f"Failed to connect to {neighbour}: {e}")
+
+
+    def send_message(self, socket, body, message_type):
         formatted_message = {
             "identity": str(self.hostname),
             "body": body,
             "type": message_type
         }
-        self.socket.send_json(formatted_message)
+        socket.send_json(formatted_message)
 
     def send_message_response(self, client_identity, body):
         formatted_message = {
@@ -109,6 +143,18 @@ class Server:
             _, identity, _, message = self.socket.recv_multipart()
             message = json.loads(message)
             identity = identity.decode("utf-8")
+            logger.info(f"Received message \"{message}\" from {identity}")
+            return [identity, message]
+
+        except zmq.error.ZMQError as e:
+            logger.error("Error receiving message:", e)
+            return None
+
+    def receive_message_neighbour(self):
+        try:
+            message = self.socket_neigh.recv_multipart()[0]
+            message = json.loads(message)
+            identity = message['identity']
             logger.info(f"Received message \"{message}\" from {identity}")
             return [identity, message]
 
@@ -133,8 +179,10 @@ class Server:
         object_exists = any(obj.get("uuid") == uuid_to_check for obj in existing_list)
 
         if object_exists:
-            # If the object exists, call the merge function
-            self.merge(new_object, new_object)
+            for i, obj in enumerate(existing_list):
+                if obj.get("uuid") == uuid_to_check:
+                    new_object = self.merge(obj, new_object)
+                    existing_list[i] = new_object
         else:
             # If the object doesn't exist, append to the ShoppingLists list
             existing_list.append(new_object)
@@ -143,8 +191,12 @@ class Server:
         with open(file_path, 'w') as file:
             json.dump(data, file, indent=2)
 
+        return new_object
+
+
+
     def merge(self, existing_data, new_data):
-        return
+        return new_data
 
     def stop(self):
         self.socket.close()
